d9bf41b8ed373ea4e6752f2730710d248a566548
kxs6629 <35939549+kxs6629@users.noreply.github.com>
2019-12-20 03:15:02 -0500

    Created Rounded Buttons and started experimenting with button functionality

--- app/src/main/java/com/example/pizzaselector/MainActivity.java
+++ app/src/main/java/com/example/pizzaselector/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.pizzaselector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button pickBtn = findViewById(R.id.startPick);
+        pickBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Make the button do something!
+                Toast.makeText(getApplicationContext(),"Test!",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
