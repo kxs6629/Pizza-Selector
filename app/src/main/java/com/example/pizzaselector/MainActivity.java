146c4c819fb3f22904ce4ae41233bb5910bf6e96
kxs6629 <35939549+kxs6629@users.noreply.github.com>
2019-12-21 00:49:43 -0500

    Removed CreateList class and added it into MainActivity as a method. Added pizzalist.txt to res->raw directory. App is now able to create a list of pizza places and display them as strings to the user on button push.

--- app/src/main/java/com/example/pizzaselector/MainActivity.java
+++ app/src/main/java/com/example/pizzaselector/MainActivity.java
@@ -7,20 +7,63 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<PizzaPlace> storeList = new ArrayList<PizzaPlace>();
+    private BufferedReader br;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InputStream is = getResources().openRawResource(R.raw.pizzalist);
+        br = new BufferedReader(new InputStreamReader(is));
+        createList(br);
 
         Button pickBtn = findViewById(R.id.startPick);
         pickBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Make the button do something!
-                Toast.makeText(getApplicationContext(),"Test!",Toast.LENGTH_SHORT).show();
+                String pickedStore = storeList.get(1).getName();
+                Toast.makeText(getApplicationContext(),pickedStore,Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void createList(BufferedReader _br){
+        try{
+            System.out.println(new File(".").getAbsoluteFile());
+            String newPlace = br.readLine();
+            while(newPlace != null){
+                String[] slices = newPlace.split(",");
+                if(slices.length == 2){
+                    PizzaPlace pp = new PizzaPlace(slices[0],slices[1]);
+                    System.out.println(pp.toString());
+                    storeList.add(pp);
+                }else if(slices.length == 3){
+                    PizzaPlace pp = new PizzaPlace(slices[0],slices[1],slices[2]);
+                    System.out.println(pp.toString());
+                    storeList.add(pp);
+                }
+                newPlace = br.readLine();
+            }
+        }catch(NullPointerException npe){
+            System.out.println("Null Pointer Exception. Check the file name!");
+            npe.printStackTrace();
+        }catch(FileNotFoundException fnfe){
+            System.out.println("Unable to locate file ");
+            fnfe.printStackTrace();
+        }catch(IOException ioe){
+            System.out.println("IOException");
+            ioe.printStackTrace();
+        }
     }
 }
